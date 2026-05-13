022eef2ea3cda339960964d1e5844fd841d6194f
rozeraize <rosezvi12@outlook.com>
2026-05-13 15:23:43 -0400

    Enhance shutdown script with cowsay notification
    Added cowsay message before shutting down WSL.

--- BatchScripts/TextBrowser.bat
+++ BatchScripts/TextBrowser.bat
@@ -5,8 +5,9 @@ echo Please wait this will only take a moment  %*
 @echo off
 bash -c "elinks" %*
 
+cls
 @echo off
-echo shutting linux off.. %*
+bash -c "cowsay -f tux 'Shutting linux down' " %*
 
 @echo off
 wsl.exe --shutdown %*
