e553560e6cbb8284be5f4c6ff8204c5dadd15207
rozeraize <rosezvi12@outlook.com>
2026-05-13 15:06:47 -0400

    Update Matrix.bat

--- BatchScripts/Matrix.bat
+++ BatchScripts/Matrix.bat
@@ -1,7 +1,12 @@
 @echo off
+echo Tip: Press 'q' to quit this application
+echo Tip: Press 'm' to cyce between matrix styles  
+echo Tip: Press 'r' to change matrix rain color %*
+pause 
+
+@echo off
 echo Loading...
 echo Please wait this will only take a moment
-echo Tip: Press 'q' to quit this application  %*
 
 cls
 @echo off
