65c6d006bc7c18778bcc493d55a2876748476bbc
rozeraize <rosezvi12@outlook.com>
2026-05-13 15:24:38 -0400

    Enhance UpdateLinux.bat with shutdown message
    Added cowsay message before shutting down WSL.

--- BatchScripts/UpdateLinux.bat
+++ BatchScripts/UpdateLinux.bat
@@ -2,6 +2,12 @@
 echo Booting linux... %*
 
 @echo off
-bash -c "sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean && sudo poweroff" %*
+bash -c "sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean" %*
+
+@echo off
+bash -c "cowsay -f tux 'Shutting linux down' " %*
+
+@echo off
+wsl.exe --shutdown %*
 
 
