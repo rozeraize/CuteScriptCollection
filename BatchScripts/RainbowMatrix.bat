da556dd4e13d1dd19a246ec67db5ab4b29a9d861
rozeraize <rosezvi12@outlook.com>
2026-05-13 15:09:29 -0400

    Update RainbowMatrix.bat

--- BatchScripts/RainbowMatrix.bat
+++ BatchScripts/RainbowMatrix.bat
@@ -1,11 +1,15 @@
 @echo off
-echo Loading...
-echo Please wait this will only take a moment
+echo Tip: Press 'm' to cycle through matrix rain styles
 echo Tip: Press 'q' to quit this application %*
+pause
 
-cls
+@echo off
+echo Loading...
+echo Please wait this will only take a moment %*
+
 @echo off
 bash -c "cmatrix | lolcat" %*
+cls
 
 @echo off
 echo shutting linux off.. %*
